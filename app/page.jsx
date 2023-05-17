1762a9a7f17f00926f38266924ca599244865a64
Ismael1699 <ismaelrodolfo15@gmail.com>
2023-05-16 19:12:32 -0600

    se genero los estilos page login

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,29 @@
+'use client';
+
 import login from './login.module.css';
+import { useState } from 'react';
 
 export default function LoginPage() {
+  const [user, setUser] = useState('Ismael Sarmiento');
+  const { password, setPassword } = useState('esta es una pasword');
+
   return (
     <div className={login.containerLogin}>
       <h1>Welcome!</h1>
-      <form>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <br />
-        <label>
-          password:
-          <input type="text" name="password" />
-        </label>
-        <input type="submit" value="Submit" />
+      <form className={login.form}>
+        <input
+          className={login.input}
+          type="text"
+          name="name"
+          placeholder="Escribe tu usuario"
+        />
+        <input
+          className={login.input}
+          type="text"
+          name="password"
+          placeholder="Escribe tu contraseña"
+        />
+        <input className={login.input} type="submit" value="Submit" />
       </form>
     </div>
   );
