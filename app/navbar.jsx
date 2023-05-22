7f8e1c92a81278fbaf2b8bec5070b64784ea3518
Ismael1699 <ismaelrodolfo15@gmail.com>
2023-05-22 16:02:51 -0600

    se implento funcionalidad al boton login de la sección user del componente navbar

--- app/navbar.jsx
+++ app/navbar.jsx
@@ -1,11 +1,21 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import style from './navbar.module.css';
 
 export default function Navbar() {
+  const router = useRouter();
+
   return (
     <div className={style.navbar}>
       <div className={style.user}>
         <div className={style.img}></div>
-        <button>Login</button>
+        <button
+          onClick={() => {
+            router.push('/login');
+          }}
+        >
+          Login
+        </button>
       </div>
       <li>
         <ul>home</ul>
