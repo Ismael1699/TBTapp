f211753e188f29dec2e04b4004e89fbbdffc762e
Ismael1699 <ismaelrodolfo15@gmail.com>
2023-05-26 10:44:10 -0600

    se agrego el nuevo metodo de cerración userLogout, el cual permite cerrar toda acceso a la aplicación

--- services/firebaseAuth.js
+++ services/firebaseAuth.js
@@ -3,13 +3,14 @@ import {
   getAuth,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signOut,
 } from 'firebase/auth';
 import router from 'next/navigation';
 
 const auth = getAuth(app);
 
 const userLogin = async (email, password) => {
-  return await signInWithEmailAndPassword(auth, email, password)
+  return signInWithEmailAndPassword(auth, email, password)
     .then(() => {
       router.push('/');
       console.log('se ha iniciado seción');
@@ -19,4 +20,14 @@ const userLogin = async (email, password) => {
     });
 };
 
-export default { userLogin };
+const userLogout = async () => {
+  return signOut(auth)
+    .then(() => {
+      console.log('la operación fue exitosa');
+    })
+    .catch((error) => {
+      console.log('sucedio un error');
+    });
+};
+
+export default { userLogin, userLogout };
