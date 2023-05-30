f3aee52c711853c723a5b8f35351255095af3b34
Ismael Sarmiento <ismaelrodolfo15@gmail.com>
2023-05-30 17:52:36 -0600

    se agrego options list de proyecto, frente y grupo de suministro

--- app/compras/agregar/page.jsx
+++ app/compras/agregar/page.jsx
@@ -6,6 +6,23 @@ export default function Agregar() {
         <option value='2103'>2103 SCT Pachuca</option>
       </select>
       <h3>Frente</h3>
+      <select name='frente'>
+        <option value='terracerias'>Terracerias</option>
+        <option value='maquinaria'>Maquinaria</option>
+        <option value='administracion'>Administración</option>
+      </select>
+      <h3>Grupo de suministro</h3>
+      <select name='grupo de suministro'>
+        <option value='materiales de construccion'>
+          Materiales de construcción
+        </option>
+        <option value='refacciones'>Refacciones</option>
+        <option value='combustibles y aceites'>Combustible y aceites</option>
+        <option value='resguardo consumo'>Resguardo cosumo</option>
+        <option value='equipo auxiliar'>Equipo auxiliar</option>
+        <option value='papeleria'>papeleria</option>
+        <option value='Otros'>Otros</option>
+      </select>
     </div>
   );
 }
