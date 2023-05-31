53acb5b29e594338ffb7779761f0c0ffe6d0dd18
Ismael Sarmiento <ismaelrodolfo15@gmail.com>
2023-05-31 13:25:20 -0600

    se agrega un boton a cuerpo de la tabla que controlara los elementos o productos que queremos requsitar a la misma, se agrego un funcion que contara los elementos y guardara las veces que hay que generar filas en la tabla

--- app/compras/agregar/page.jsx
+++ app/compras/agregar/page.jsx
@@ -1,48 +1,74 @@
+'use client';
+import { useState } from 'react';
 import style from './agregar.module.css';
 
 export default function Agregar() {
+  const [itemTable, setItemTable] = useState([0]);
+
+  function addRowTable() {
+    setItemTable([...itemTable, itemTable[setItemTable.length - 1] + 1]);
+  }
+
+  console.log(itemTable);
+
   return (
     <div>
-      <h3>Proyecto</h3>
-      <select name='proyecto'>
-        <option value='2103'>2103 SCT Pachuca</option>
-      </select>
+      <div className={style.head}>
+        <div className={style.proyecto}>
+          <h5>Proyecto</h5>
+          <select name='proyecto'>
+            <option value='2103'>2103 SCT Pachuca</option>
+          </select>
+        </div>
 
-      <h3>Frente</h3>
-      <select name='frente'>
-        <option value='terracerias'>Terracerias</option>
-        <option value='maquinaria'>Maquinaria</option>
-        <option value='administracion'>Administración</option>
-      </select>
+        <div className={style.frente}>
+          <h5>Frente</h5>
+          <select name='frente'>
+            <option value='terracerias'>Terracerias</option>
+            <option value='maquinaria'>Maquinaria</option>
+            <option value='administracion'>Administración</option>
+          </select>
+        </div>
 
-      <h3>Grupo de suministro</h3>
-      <select name='grupo de suministro'>
-        <option value='materiales de construccion'>
-          Materiales de construcción
-        </option>
-        <option value='refacciones'>Refacciones</option>
-        <option value='combustibles y aceites'>Combustible y aceites</option>
-        <option value='resguardo consumo'>Resguardo cosumo</option>
-        <option value='equipo auxiliar'>Equipo auxiliar</option>
-        <option value='papeleria'>papeleria</option>
-        <option value='Otros'>Otros</option>
-      </select>
+        <div className={style.suministro}>
+          <h5>Grupo de suministro</h5>
+          <select name='grupo de suministro'>
+            <option value='materiales de construccion'>
+              Materiales de construcción
+            </option>
+            <option value='refacciones'>Refacciones</option>
+            <option value='combustibles y aceites'>
+              Combustible y aceites
+            </option>
+            <option value='resguardo consumo'>Resguardo cosumo</option>
+            <option value='equipo auxiliar'>Equipo auxiliar</option>
+            <option value='papeleria'>papeleria</option>
+            <option value='Otros'>Otros</option>
+          </select>
+        </div>
 
-      <h3>Fecha</h3>
-      <input type='date'></input>
+        <div className={style.fecha}>
+          <h5>Fecha</h5>
+          <input type='date'></input>
+        </div>
 
-      <h3>Lugar de compra</h3>
-      <select name='lugar de compra'>
-        <option value='local'>Compra local</option>
-        <option value='regional'>Compra regional</option>
-        <option value='nacional'>Compra nacional</option>
-      </select>
+        <div className={style.lugar}>
+          <h5>Lugar de compra</h5>
+          <select name='lugar de compra'>
+            <option value='local'>Compra local</option>
+            <option value='regional'>Compra regional</option>
+            <option value='nacional'>Compra nacional</option>
+          </select>
+        </div>
 
-      <h3>Provedor</h3>
-      <select name='proverdores'>
-        <option value='provedor1'>Provedor 1</option>
-        <option value='provedor2'>Provedor 2</option>
-      </select>
+        <div className={style.proveedor}>
+          <h5>Proveedor</h5>
+          <select name='proverdores'>
+            <option value='provedor1'>Provedor 1</option>
+            <option value='provedor2'>Provedor 2</option>
+          </select>
+        </div>
+      </div>
 
       <table className={style.table}>
         <tr>
@@ -53,8 +79,15 @@ export default function Agregar() {
           <th>Cantidad</th>
           <th>Precio Unitario</th>
           <th>Precio Final</th>
+          <th>modos</th>
         </tr>
       </table>
+
+      <button
+        className={style.button}
+        onClick={addRowTable}>
+        agregar
+      </button>
     </div>
   );
 }
