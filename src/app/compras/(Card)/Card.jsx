ca1aadc5b18bb35f95bcb1a6678061d5b0d8c8d7
Ismael Sarmiento <ismaelrodolfo15@gmail.com>
2023-07-14 09:39:42 -0600

    se implemento un nuevo diseño para las tarjetas de las ordenes de compra

--- src/app/compras/(Card)/Card.jsx
+++ src/app/compras/(Card)/Card.jsx
@@ -1,43 +1,94 @@
 import Link from 'next/link';
 import style from './card.module.css';
 import DownloadButton from './DownloadButton/DownloadButton';
+import SegmentBarStatus from './SegmentBarStatus/SegmentBarStatus';
+
+const fechaname = [
+  'enero',
+  'febrero',
+  'marzo',
+  'abril',
+  'mayo',
+  'junio',
+  'julio',
+  'agosto',
+  'septiembre',
+  'octubre',
+  'noviembre',
+  'diciembre',
+];
 
 export default async function Card({ obj }) {
+  const arrayFrente = obj.frente.split('').map((item, index) => {
+    if (index === 0) {
+      return item;
+    }
+    return item.toLowerCase();
+  });
+  const frenteLowerCase = arrayFrente.join('');
+  const arrayFecha = obj.fecha.split('-');
+
   return (
     <div className={style.card}>
-      <p className={style.proyecto}>{obj.proyecto}</p>
-      <p className={style.no}>No. {obj.numero}</p>
-      <p className={style.departamento}>{obj.frente}</p>
-      <p className={style.fecha}>{obj.fecha} </p>
-      <div className={style.descripcion}>
-        <p>
-          Descripción
-          <br />
+      <div className={style.header}>
+        <div className={style.downloadIcon}>
+          <i className='bi bi-file-earmark-arrow-down-fill'></i>
+        </div>
+        <div className={style.title}>
+          <p className={style.orden}>Orden de compra {obj.numero}</p>
+          <p className={style.fecha}>
+            {arrayFecha[2]} de {fechaname[parseInt(arrayFecha[1], 10) - 1]} de{' '}
+            {arrayFecha[0]}
+          </p>
+        </div>
+        <div className={style.options}>
+          <i className='bi bi-three-dots'></i>
+        </div>
+      </div>
+      <div className={style.body}>
+        <div className={style.frente}>
+          <p>{frenteLowerCase}</p>
+          <p>{obj.proyecto}</p>
+        </div>
+        <p className={style.cantidad}>{obj.obj_table.table[0].cantidad}</p>
+        <p className={style.descripcion}>
           {obj.obj_table.table[0].descripcion}
         </p>
-        <p>
-          precio <br />
-          {parseInt(obj.obj_table.table[0].unitario).toLocaleString('en', {
+        <p className={style.unitario}>
+          {(
+            parseInt(obj.obj_table.table[0].unitario) *
+            parseInt(obj.obj_table.table[0].cantidad)
+          ).toLocaleString('en', {
+            style: 'currency',
+            currency: 'MXN',
+          })}
+        </p>
+        <div className={style.more1}>
+          <i className='bi bi-three-dots'></i>
+        </div>
+        <div className={style.more2}>
+          <i className='bi bi-three-dots'></i>
+        </div>
+        <p className={style.total}>
+          {parseInt(obj.obj_table.table[0].final).toLocaleString('en', {
             style: 'currency',
             currency: 'MXN',
           })}
         </p>
       </div>
-      <p className={style.requisito}>
-        solicitante:
-        <br /> {obj.frente}
-      </p>
       <div className={style.status}>
-        {/* <h3 className={style.statustitle}>status</h3>
-          <div className={style.barsubida}></div>
-          <div className={style.barautorizada1}></div>
-          <div className={style.barautorizada2}></div>
-          <div className={style.barpagada}></div> */}
-        <Link href={process.env.URL_HOST + '/compras/' + obj.id}>
-          <button>Editar</button>
-        </Link>
-        <DownloadButton obj={obj} />
+        <SegmentBarStatus formSegment='initial' />
+        <SegmentBarStatus formSegment='center' />
+        <SegmentBarStatus formSegment='center' />
+        <SegmentBarStatus formSegment='final' />
       </div>
     </div>
   );
 }
+
+// {
+//   parseInt(obj.obj_table.table[0].unitario).toLocaleString('en', {
+//     style: 'currency',
+//     currency: 'MXN',
+//   });
+// }
