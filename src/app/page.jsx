78bb25f493218d4062ee6aa2f156e6ce41a6de6c
Ismael Sarmiento <ismaelrodolfo15@gmail.com>
2023-07-26 12:33:37 -0600

    se cambio la presentacion de la app, ahora es mas visual

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,17 +6,15 @@ export default async function App() {
   return (
     <div className={style.home}>
       <p>Triturados Basálticos Tepetlaoxtoc</p>
-      <div className={style.img}>
-        {/* <Image
-          src='/IMG_0428.jpg'
-          alt='criba'
-          fill={true}
-          priority={true}
-          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-        /> */}
-      </div>
 
-      <button className='button'>
+      {/* <p>
+        En TBT, hemos diseñado una app poderosa y versátil para simplificar y
+        optimizar la gestión de nuestos proyectos. Nuestra app está
+        especialmente diseñada para proyectos en la rama de la construcción y
+        mantenimiento de carreteras, adaptando nuestros procesos a la era
+        digital.
+      </p> */}
+      <button className={style.button}>
         <Link href='/application/compras'>Comenzar</Link>
       </button>
     </div>
