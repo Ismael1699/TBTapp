c864cb9255a615272d022036c2fbcd7c4f6e0f92
Ismael Sarmiento <ismaelrodolfo15@gmail.com>
2023-08-08 12:04:08 -0600

    se arreglo problema al cargar el favicon.ico

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -5,9 +5,3 @@ export const lato = Lato({
   subsets: ['latin'],
   display: 'swap',
 });
-
-// export const roboto_mono = Releway({
-//   weight: ['100', '300', '400', '700', '900'],
-//   subsets: ['latin'],
-//   display: 'swap',
-// });
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,10 @@
 import style from './home.module.css';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default async function App() {
   return (
     <div className={style.home}>
       <p>Triturados Basálticos Tepetlaoxtoc</p>
-
-      {/* <p>
-        En TBT, hemos diseñado una app poderosa y versátil para simplificar y
-        optimizar la gestión de nuestos proyectos. Nuestra app está
-        especialmente diseñada para proyectos en la rama de la construcción y
-        mantenimiento de carreteras, adaptando nuestros procesos a la era
-        digital.
-      </p> */}
       <button className={style.button}>
         <Link href='/application'>Comenzar</Link>
       </button>
