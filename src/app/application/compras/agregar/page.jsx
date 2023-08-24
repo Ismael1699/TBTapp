7fcea929f0626b1f3afe735f8d51b2cf10c705de
Ismael Sarmiento <ismaelrodolfo15@gmail.com>
2023-08-23 18:23:06 -0600

    Se corrigió problema al renderizar el componente AgregarCompra en la page de agrega compra, los datos requeridos ahora los suministra el servidor y no se consultan en el cliente

--- src/app/application/compras/agregar/page.jsx
+++ src/app/application/compras/agregar/page.jsx
@@ -1,106 +1,22 @@
-'use client';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import HeadInputs from '@/components/Compras/agregar/HeadInputs/HeadInputs';
-import dynamic from 'next/dynamic';
+import AgregarCompra from '@/components/Compras/agregar/AgregarCompra';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import axios from 'axios';
 
-const Table = dynamic(
-  () => import('@/components/Compras/agregar/Table/Table'),
-  { ssr: false }
-);
-
-const structHead = {
-  proyecto: '',
-  frente: '',
-  suministro: '',
-  fecha: '',
-  lugar: '',
-  proveedor: '',
-  numero: '',
-};
-
-async function sendBackend(data) {
-  return await fetch(`/api/compras`, {
-    method: 'POST',
-    body: JSON.stringify(data),
-  });
+async function getProveedores(rol) {
+  const res = await axios(
+    process.env.NEXT_PUBLIC_URL_HOST + `/api/compras/proveedores?rol=${rol}`
+  );
+  return res.data;
 }
 
-export default function Agregar() {
-  const [headData, setHeadData] = useState(structHead);
-  const [dataProveedor, setDataProveedor] = useState({});
-  const [isEditing, setIsEditing] = useState(false);
-  const [items, setItems] = useState([]);
-  const router = useRouter();
-
-  console.log(dataProveedor);
-
-  function centralizeData() {
-    let rowContentData = false;
-
-    if (items.length === 0) {
-      rowContentData = false;
-    } else {
-      items.map((obj) => {
-        const valuesObj = Object.values(obj);
-        valuesObj.map((values) =>
-          values === '' || values === 0 || values === '0'
-            ? (rowContentData = false)
-            : (rowContentData = true)
-        );
-      });
-    }
-    const value = Object.values(headData);
-    let headContentData = true;
-    value.map((item) =>
-      item === '' ? (headContentData = false) : (headContentData = true)
-    );
-
-    if (rowContentData && headContentData) {
-      const precioTotal = parseFloat(
-        items.reduce((acumulador, obj) => {
-          return acumulador + parseFloat(parseFloat(obj.final).toFixed(2));
-        }, 0)
-      ).toFixed(2);
-
-      const precioFinal =
-        dataProveedor.factura === 1
-          ? parseFloat(precioTotal * 1.16).toFixed(2)
-          : precioTotal;
-
-      const data = { ...headData, precio: precioFinal, table: items };
-      return saveData(data);
-    } else {
-      alert('Por favor terminina de llenar los datos');
-    }
-  }
-
-  async function saveData(data) {
-    const res = await sendBackend(data);
-
-    if (res.ok) {
-      const res1 = JSON.parse(await res.text());
-      alert(res1.message);
-      router.push('/application/compras');
-    } else {
-      alert('Status: ' + res.status + ' ' + res.statusText);
-    }
-  }
-
+export default async function Agregar() {
+  const session = await getServerSession(authOptions);
+  const proveedores = await getProveedores(session.user.rol);
   return (
-    <div>
-      <HeadInputs
-        headData={headData}
-        setHeadData={setHeadData}
-        isEditing={isEditing}
-        setDataProveedor={setDataProveedor}
-      />
-      <Table
-        items={items}
-        setItems={setItems}
-        centralizeData={centralizeData}
-        isEditing={isEditing}
-      />
-    </div>
+    <AgregarCompra
+      proveedores={proveedores}
+      user={session.user}
+    />
   );
 }
---
+++ src/components/Compras/agregar/AgregarCompra.jsx
@@ -0,0 +1,106 @@
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import HeadInputs from '@/components/Compras/agregar/HeadInputs/HeadInputs';
+import dynamic from 'next/dynamic';
+
+const Table = dynamic(
+  () => import('@/components/Compras/agregar/Table/Table'),
+  { ssr: false }
+);
+
+const structHead = {
+  proyecto: '',
+  frente: '',
+  suministro: '',
+  fecha: '',
+  lugar: '',
+  proveedor: '',
+  numero: '',
+};
+
+async function sendBackend(data) {
+  return await fetch(`/api/compras`, {
+    method: 'POST',
+    body: JSON.stringify(data),
+  });
+}
+
+export default function AgregarCompra({ proveedores, user }) {
+  const [headData, setHeadData] = useState(structHead);
+  const [dataProveedor, setDataProveedor] = useState({});
+  const [isEditing, setIsEditing] = useState(false);
+  const [items, setItems] = useState([]);
+  const router = useRouter();
+
+  function centralizeData() {
+    let rowContentData = false;
+
+    if (items.length === 0) {
+      rowContentData = false;
+    } else {
+      items.map((obj) => {
+        const valuesObj = Object.values(obj);
+        valuesObj.map((values) =>
+          values === '' || values === 0 || values === '0'
+            ? (rowContentData = false)
+            : (rowContentData = true)
+        );
+      });
+    }
+    const value = Object.values(headData);
+    let headContentData = true;
+    value.map((item) =>
+      item === '' ? (headContentData = false) : (headContentData = true)
+    );
+
+    if (rowContentData && headContentData) {
+      const precioTotal = parseFloat(
+        items.reduce((acumulador, obj) => {
+          return acumulador + parseFloat(parseFloat(obj.final).toFixed(2));
+        }, 0)
+      ).toFixed(2);
+
+      const precioFinal =
+        dataProveedor.factura === 1
+          ? parseFloat(precioTotal * 1.16).toFixed(2)
+          : precioTotal;
+
+      const data = { ...headData, precio: precioFinal, table: items };
+      return saveData(data);
+    } else {
+      alert('Por favor terminina de llenar los datos');
+    }
+  }
+
+  async function saveData(data) {
+    const res = await sendBackend(data);
+
+    if (res.ok) {
+      const res1 = JSON.parse(await res.text());
+      alert(res1.message);
+      router.push('/application/compras');
+    } else {
+      alert('Status: ' + res.status + ' ' + res.statusText);
+    }
+  }
+
+  return (
+    <div>
+      <HeadInputs
+        headData={headData}
+        setHeadData={setHeadData}
+        isEditing={isEditing}
+        setDataProveedor={setDataProveedor}
+        proveedores={proveedores}
+        user={user}
+      />
+      <Table
+        items={items}
+        setItems={setItems}
+        centralizeData={centralizeData}
+        isEditing={isEditing}
+      />
+    </div>
+  );
+}
